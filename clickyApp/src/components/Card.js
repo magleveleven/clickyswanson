bddb71982aff1af029c6bcedff5839deda5fee60
Eric Ballesteros <ballesteros@icloud.com>
2019-09-08 15:15:18 -0500

    added card componenet

---
+++ clickyApp/src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Card extends React.Component {
+    render () {
+        return (
+            <div className="imageArea">
+              <div className="testarea">
+                image area hopefully
+              </div>
+              </div>
+              )
+    }
+};
+
+export default Card;
