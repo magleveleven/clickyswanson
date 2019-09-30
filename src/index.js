1a07858110797507adc661c3217a59d053349149
Eric Ballesteros <ballesteros@icloud.com>
2019-09-30 14:06:56 -0500

    debugging heroku connect

--- src/index.js
+++ src/index.js
@@ -7,14 +7,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'materialize-css/dist/css/materialize.min.css';
 
-var PORT = process.env.PORT || 5000;
-var express = require('express');
-var app = express();
-
-
-app.listen(PORT, function(){
-    console.log("clicky server running")
-});
 
 
 ReactDOM.render(<App />, document.getElementById('main'));
