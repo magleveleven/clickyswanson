9b2588f45df64de939e5335d3dad9c0c8c32398d
Eric Ballesteros <ballesteros@icloud.com>
2019-09-30 12:21:30 -0500

    reordered index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+import * as serviceWorker from './serviceWorker';
+import 'materialize-css/dist/css/materialize.min.css';
+
 var PORT = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
@@ -10,14 +19,6 @@ server.listen(PORT, function() {
     console.log("clicky server running");
 });
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-
-import * as serviceWorker from './serviceWorker';
-import 'materialize-css/dist/css/materialize.min.css';
 
 ReactDOM.render(<App />, document.getElementById('main'));
 
