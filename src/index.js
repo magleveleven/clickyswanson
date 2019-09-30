9215eecf6471e1a76a17b7a26607677963076a3a
Eric Ballesteros <ballesteros@icloud.com>
2019-09-30 12:51:32 -0500

    bug no mod found

--- src/index.js
+++ src/index.js
@@ -11,12 +11,9 @@ var PORT = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
 
-var http = require('http');
-var server = http.Server(app);
 
-app.use(express.static('client'));
-server.listen(PORT, function() {
-    console.log("clicky server running");
+app.listen(PORT, function(){
+    console.log("clicky server running")
 });
 
 
