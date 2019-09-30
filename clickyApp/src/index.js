b279e0c3633c2516b4df3eca3801c83049c4ff74
Eric Ballesteros <ballesteros@icloud.com>
2019-09-30 11:54:46 -0500

    updated port

--- clickyApp/src/index.js
+++ clickyApp/src/index.js
@@ -1,3 +1,15 @@
+var PORT = process.env.PORT || 5000;
+var express = require('express');
+var app = express();
+
+var http = require('http');
+var server = http.Server(app);
+
+app.use(express.static('client'));
+server.listen(PORT, function() {
+    console.log("clicky server running");
+});
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
